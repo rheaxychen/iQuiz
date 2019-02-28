832f5e0ec2c3c1ab68054894cfbec6a09a2b3c21
Rhea X Chen <xinyuc2@uw.edu>
2019-02-28 11:52:53 -0800

    Add initial files

---
+++ iQuiz/iQuiz/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  iQuiz
+//
+//  Created by Rhea on 2/28/19.
+//  Copyright © 2019 Rhea. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view, typically from a nib.
+	}
+
+
+}
+
